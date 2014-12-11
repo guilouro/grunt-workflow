bd6693a96c455b4cb3408abf9ca46c06210363a6
Guilherme Louro <guilherme-louro@hotmail.com>
2014-12-11 14:46:14 -0200

    Banner

--- _grunt/uglify.js
+++ _grunt/uglify.js
@@ -1,4 +1,18 @@
 module.exports = {
+	options: {
+		banner:
+        "\n" +
+        "/*\n" +
+        " * -------------------------------------------------------\n" +
+        " * Project: <%= pkg.title %>\n" +
+        " * Version: <%= pkg.version %>\n" +
+        " * Author:  <%= pkg.author.name %> (<%= pkg.author.email %>)\n" +
+        " *\n" +
+        " * Copyright (c) <%= grunt.template.today(\"yyyy\") %> <%= pkg.title %>\n" +
+        " * -------------------------------------------------------\n" +
+        " */\n" +
+        "\n"
+	},
 	dist: {
 		files: {
 			'<%= path.dest %>js/all.min.js': ['<%= path.src %>js/main.js']
