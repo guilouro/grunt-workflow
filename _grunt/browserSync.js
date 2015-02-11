101a75ce7b49324ca8d4a5e26846010ff69e52ac
Guilherme Louro <guilherme-louro@hotmail.com>
2015-02-11 10:55:34 -0200

    replace tabs

--- _grunt/browserSync.js
+++ _grunt/browserSync.js
@@ -1,18 +1,18 @@
 module.exports = {
-	dev: {
+    dev: {
         files: {
             src: [
                 '<%= path.dest %>css/*.css',
                 '<%= path.dest %>*.html',
             ]
         },
-		options: {
-			watchTask: true,
+        options: {
+            watchTask: true,
             ghostMode: {
                 scroll: true,
                 links: true,
                 forms: true
             }
-		},
-	}
+        },
+    }
 };
