f723ce3c688d6eb807aa38808ea1c93569cdb4ea
Guilherme Louro <guilherme-louro@hotmail.com>
2015-02-11 10:52:14 -0200

    added browsersync

---
+++ _grunt/browserSync.js
@@ -0,0 +1,18 @@
+module.exports = {
+	dev: {
+        files: {
+            src: [
+                '<%= path.dest %>css/*.css',
+                '<%= path.dest %>*.html',
+            ]
+        },
+		options: {
+			watchTask: true,
+            ghostMode: {
+                scroll: true,
+                links: true,
+                forms: true
+            }
+		},
+	}
+};
